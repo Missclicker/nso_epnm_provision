78ce30be2b0ac6bdcd47c9ff2e86cb4dc6be5060
Missclicker <85ping@gmail.com>
2022-07-08 09:51:40 +0300

    finish frontend with search and dynamic buttons

--- bs_app.py
+++ bs_app.py
@@ -11,15 +11,32 @@ FILE = 'Data_files/crossing_test.xlsx'
 DEPLOY_FILE = 'bs_status.csv'
 DEPLOY_STATUS = pd.read_csv(DEPLOY_FILE, index_col=['bs_id', 'vlan'])
 DEPLOY_STATUS.deployed_on = DEPLOY_STATUS.deployed_on.astype(str)
+DF = normalize_crossing(FILE)
+DF['deployed_on'] = DEPLOY_STATUS
+DF = DF.reset_index()
 
 @app.route('/')
 def index():
-    return render_template("test.html", column_names=df.columns.values, row_data=list(df.values.tolist()),
-                           link_column="BS", zip=zip, title='BS Deploy table')
+    return render_template(
+        "test.html",
+        column_names=DF[[
+            'BS', 'vlan', 'bs_type', 'service', 'description', 'ncs', 'port', 'deployed_on'
+        ]].columns.values,
+        row_data=list(DF.values.tolist()),
+        link_column="BS", zip=zip, title='BS Deploy table'
+    )
+
+
+# @app.route('/data/<bs_id>')
+# def data(bs_id):
+#
+
+@app.route('/api/data')
+def data():
+    return {'data': DF[[
+        'BS', 'vlan', 'bs_type', 'service', 'description', 'ncs', 'port', 'deployed_on'
+    ]].fillna("").to_dict(orient='records')}
 
 
 if __name__ == '__main__':
-    df = normalize_crossing(FILE)
-    df['deployed_on'] = DEPLOY_STATUS
-    df = df.reset_index()
     app.run(debug=True, port=4949)
